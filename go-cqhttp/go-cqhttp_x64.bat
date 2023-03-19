78ce16775fff332337662edb7c76550e4af13864
iidamie <2279897399@qq.com>
2023-03-19 14:14:47 +0800

    上传内容

---
+++ go-cqhttp/go-cqhttp_x64.bat
@@ -0,0 +1,2 @@
+%Created by go-cqhttp. DO NOT EDIT ME!%
+start cmd /K "go-cqhttp_windows_x64.exe"
---
+++ src/plugins/ping.py
@@ -0,0 +1,13 @@
+"""超级用户 Ping 指令回复，可以用来检查 Bot 部署状态"""
+
+from nonebot import on_command
+from nonebot.permission import SUPERUSER
+from nonebot.matcher import Matcher
+from nonebot.internal.adapter import Event
+
+h_ping = on_command("ping", aliases={"Ping", "PING"}, permission=SUPERUSER)
+
+
+@h_ping.handle()
+async def _(matcher: Matcher, _: Event):
+    await matcher.finish("Pong~")
